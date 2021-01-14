4972daf536a30fa8fd6e766ced2f70dbc67cbee7
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-01-14 18:02:52 +0100

    Create app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent
   ],
   imports: [
     BrowserModule,
