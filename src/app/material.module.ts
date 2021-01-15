b6a9662f3a2aac6dc17ffed5f1fa7b025f32b199
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-01-15 19:24:51 +0100

    Add datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,12 +4,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import  {MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
 const modulesArray = [
   MatButtonModule,
   MatIconModule,
   MatFormFieldModule,
   MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 ]
 
 @NgModule({
